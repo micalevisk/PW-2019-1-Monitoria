a67c4460bbd2012d3940a9d332f154b7c1037e57
Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>
2019-04-03 23:02:11 -0400

    add: script para adicionar um novo "git submodule" de acordo

---
+++ git-add-submodule.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+[ $# -lt 2 ] && { echo "USAGE: $0 <username> [turma]"; exit 1; }
+
+username="${1,,}"
+turma="${2%%/*}"
+
+[ -d "./${turma}" ] || { echo "'./${turma}/' is not a directory." ; exit 2; }
+
+git rm --cached "./${turma}/${username}"
+
+git submodule add -b master \
+  "https://github.com/${username}/ProgWeb" \
+  "./${turma}/${username}"
